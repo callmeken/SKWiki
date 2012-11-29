38e753d9abb7a3029bed2ff006343cb560adc9f5
Justin <justin@zentnmd.com>
2012-11-28 19:04:27 -0800

    push for demo

--- www/sites/all/themes/skwiki/templates/page.tpl.php
+++ www/sites/all/themes/skwiki/templates/page.tpl.php
@@ -1,12 +1,9 @@
 <div id="page" class="container <?php print $classes; ?>">
-
-  <?php print render($page['top_menu']); ?>
-
   <header id="header" class="clearfix" role="banner">
 
     <?php if ($site_logo || $site_name || $site_slogan): ?>
       <!-- start: Branding -->
-      <div id="branding" class="branding-elements clearfix">
+      <div id="branding" class="branding-elements">
 
         <?php if ($site_logo): ?>
           <div id="logo">
@@ -29,36 +26,20 @@
           </hgroup><!-- /end #name-and-slogan -->
 
         <?php endif; ?>
-
+      
+      <div class="clear"></div>
       </div><!-- /end #branding -->
     <?php endif; ?>
 
     <?php print render($page['header']); ?>
     <?php print render($page['menu_bar']); ?>
-
+    <div class="clear"></div>
   </header>
   
-  <?php if ($primary_local_tasks || $secondary_local_tasks || $action_links): ?>
-    <div id="tasks">
-
-      <?php if ($primary_local_tasks): ?>
-        <ul class="tabs primary clearfix"><?php print render($primary_local_tasks); ?></ul>
-      <?php endif; ?>
-
-      <?php if ($secondary_local_tasks): ?>
-        <ul class="tabs secondary clearfix"><?php print render($secondary_local_tasks); ?></ul>
-      <?php endif; ?>
-
-      <?php if ($action_links = render($action_links)): ?>
-        <ul class="action-links clearfix"><?php print $action_links; ?></ul>
-      <?php endif; ?>
-      
-    </div>
-  <?php endif; ?>
-
+  <?php print render($page['top_menu']); ?>
+  
   <div id="columns"<?php print $page['menu_bar'] ? 'class="no-menu-bar"' : '' ;?>>
     <div class="columns-inner clearfix">
-
       <?php print $messages; ?>
       <?php print render($page['help']); ?>
       <?php print render($page['secondary_content']); ?>
@@ -80,7 +61,7 @@
         </div>
       <?php endif; ?>
 
-      <?php if ($breadcrumb): print $breadcrumb; endif; ?>
+      <?php //if ($breadcrumb): print $breadcrumb; endif; ?>
 
       <div id="content-column">
         <div class="content-inner">
@@ -88,7 +69,25 @@
           <?php print render($page['highlighted']); ?>
 
           <<?php print $tag; ?> id="main-content" role="main">
-
+          
+           <?php if ($primary_local_tasks || $secondary_local_tasks || $action_links): ?>
+            <div id="tasks">
+        
+              <?php if ($primary_local_tasks): ?>
+                <ul class="tabs primary clearfix"><?php print render($primary_local_tasks); ?></ul>
+              <?php endif; ?>
+        
+              <?php if ($secondary_local_tasks): ?>
+                <ul class="tabs secondary clearfix"><?php print render($secondary_local_tasks); ?></ul>
+              <?php endif; ?>
+        
+              <?php if ($action_links = render($action_links)): ?>
+                <ul class="action-links clearfix"><?php print $action_links; ?></ul>
+              <?php endif; ?>
+              
+            </div>
+          <?php endif; ?>
+      
             <?php print render($title_prefix); ?>
             <?php if ($title || $primary_local_tasks || $secondary_local_tasks || $action_links = render($action_links)): ?>
               <header id="main-content-header">
